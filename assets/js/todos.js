ec7ac4bd4230987b59603742785319d8c0781341
smwq <smwaqarquadri@gmail.com>
2018-09-05 18:19:20 +0530

    Initial commit

---
+++ assets/js/todos.js
@@ -0,0 +1,27 @@
+//check off specific Todos by clicking
+$("ul").on("click","li", function(){
+	$(this).toggleClass("completed");
+});
+
+//Click on X to delete Todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if (event.which === 13)
+	{
+		//grabbing new todo text from input
+		var toDoText = $(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + toDoText + "</li>");
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
